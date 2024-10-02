3f814b527e0e05e6521e62d6879d2586371c18a7
hugocharels <hugocharels@hotmail.com>
2024-10-02 22:49:25 +0200

    :rocket: quick select working but lazy not

--- assignment_1_selection/select.py
+++
@@ -1,7 +0,0 @@
-def quick_select(arr: [int], k: int) -> int:
-    ...
-
-
-def lazy_select(arr: [int], k: int) -> int:
-    ...
-
--- assignment_1_selection/test_select.py
+++ assignment_1_selection/test_select.py
@@ -1,8 +1,40 @@
-import select
+from src import select
+import pytest
+from random import randint
+
+SIZE = 1000
+TRY = 0
 
 
-def test_basic():
+PARAMS = [
+	([randint(1, 1000) for _ in range(SIZE)], randint(0, 999))
+	for _ in range(TRY)
+]
+
+
+def correct_select(arr, k):
+	return sorted(arr)[k]
+
+
+def test_basic_quick():
 	arr = [1, 2, 3]
-	k = 2
+	k = 1
 	assert select.quick_select(arr, k) == 2
+
+
+def test_basic_lazy():
+	arr = [1, 2, 3]
+	k = 1
 	assert select.lazy_select(arr, k) == 2
+
+
+@pytest.mark.parametrize("param", PARAMS)
+def test_random_arr_quick(param):
+	arr, k = param
+	assert select.quick_select(arr, k) == correct_select(arr, k)
+
+
+@pytest.mark.parametrize("param", PARAMS)
+def test_random_arr_lazy(param):
+	arr, k = param
+	assert select.lazy_select(arr, k) == correct_select(arr, k)
