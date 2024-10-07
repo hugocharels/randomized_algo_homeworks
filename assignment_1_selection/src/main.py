c82ecb0b0786fd962b2f3db6cf7b04ecf2b9b431
hugocharels <hugocharels@hotmail.com>
2024-10-07 13:07:34 +0000

    a

--- assignment_1_selection/src/main.py
+++ assignment_1_selection/src/main.py
@@ -2,8 +2,11 @@ from select import quick_select, lazy_select
 from random import shuffle
 
 if __name__ == '__main__':
-	arr = [*range(1, 1001)]
-	shuffle(arr)
-	k = 500
-	print(quick_select(arr, k))
-	print(lazy_select(arr, k))
+    arr = [Integer(x) for x in range(1000)]
+    shuffle(arr)
+    k = 500
+    print(quick_select(arr, k))
+    print(f"quick : {Integer.COMPARISON_COUNT}")
+    Integer.clear(_comparison)
+    print(lazy_select(arr, k))
+    print(f"lazy : {Integer.COMPARISON_COUNT}")
