4c4eff70b6cc84ebf41fdaa2ffbad0d3e159fb83
hugocharels <hugocharels@hotmail.com>
2024-12-17 18:53:00 +0100

    :zap: add parallelization into the main function

--- assignment_2_karger/src/main.rs
+++ assignment_2_karger/src/main.rs
@@ -1,19 +1,31 @@
 use assignment_2_karger::data_generator::*;
+use rayon::prelude::*;
 
 fn main() {
-	generate_data(
-		vec![100,200,300,400,500,600,700,800,900,1000],
-		|v: usize| 2*v,
-		"generated_data/hundred_2v.csv"
-	);
-	generate_data(
-		vec![100,200,300,400,500,600,700,800,900,1000],
-		|v: usize| 5*v,
-		"generated_data/hundred_5v.csv"
-	);
-	generate_data(
-		vec![100,200,300,400,500,600,700,800,900,1000],
-		|v: usize| 10*v,
-		"generated_data/hundred_10v.csv"
-	);
+	// Define the operations and their corresponding file names
+	let tasks:Vec<(fn(usize)->usize, &str)>  = vec![
+		(
+			|v: usize| 2 * v,
+			"generated_data/hundred_2v.csv",
+		),
+		(
+			|v: usize| 5 * v,
+			"generated_data/hundred_5v.csv",
+		),
+		(
+			|v: usize| 10 * v,
+			"generated_data/hundred_10v.csv",
+		),
+	];
+
+	// Run each task in parallel
+	tasks.into_par_iter().for_each(|(operation, filename)| {
+		println!("Generating data for {}", filename);
+		generate_data(
+			vec![100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+			operation,
+			filename,
+		);
+		println!("Data generated for {}", filename);
+	});
 }
